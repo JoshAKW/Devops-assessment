ca7c007356780bc5e007620ae7b38558311428e5
Joshua West <JoshuaHWest@gmail.com>
2022-07-21 20:32:53 -0600

    ready for review

--- heroku.md
+++ heroku.md
@@ -1,2 +1,3 @@
 ## Paste your link below
+https://dashboard.heroku.com/apps/assessment-6-devops/deploy/github
 
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors =require('cors')
 const path = require('path')
 const app = express()
 const {bots, playerRecord} = require('./data')
 const {shuffleArray} = require('./utils')
 
 app.use(express.json())
+app.use(cors())
 
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
